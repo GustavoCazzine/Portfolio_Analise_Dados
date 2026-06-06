d43db9a5b806547cadf831b52fcf6ad358a405b4
Gustavo Cazzine <cazzinengustavo@gmail.com>
2026-06-06 16:31:56 -0300

    feat: implementacao de regras de data cleaning com pandas

--- 01_ETL_Mercado_Livre/src/transformador.py
+++ 01_ETL_Mercado_Livre/src/transformador.py
@@ -7,7 +7,6 @@ class TransformardorDados:
 
 
     def transformar_e_salvar(self):
-
         dados_limpos = []
         resultados = self.dados_brutos
 
@@ -17,11 +16,15 @@ class TransformardorDados:
 
                     dados_limpos.append({"Produto" : titulo, "Preço" : preco})
 
-
         df = pd.DataFrame(dados_limpos)
 
+        # 1. Força a conversão para número e transforma lixo em NaN
+        df['Preço'] = pd.to_numeric(df['Preço'], errors='coerce')
+        # 2. Deleta as linhas onde o preço ficou vazio (NaN)
+        df = df.dropna(subset=['Preço'])
+        # 3. Remove produtos com nomes exatamente iguais
+        df = df.drop_duplicates(subset=['Produto'])
+
         caminho_arquivo = f"data/processed/extrator_produtos.csv"
-
         df.to_csv(caminho_arquivo, index=False)
-
-        print("Arquivo salvo com sucesso!")
+        print("Arquivo salvo e limpo com sucesso!")
